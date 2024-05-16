45057a390d3dfdc891795468a62065e2779a7caf
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-05-16 18:05:44 +0200

    started

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1 className="bg-gray-600 px-6 py-3 font-bold text-1xl">About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1 className="bg-red-600 px-6 py-3 font-bold text-1xl ">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
